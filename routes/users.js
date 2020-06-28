160234d777589e2382cbc99b551b16a3e201e493
Alex Rueb <rueba@oregonstate.edu>
2020-06-28 10:51:07 -0500

    cleaned up formatting

--- routes/users.js
+++ routes/users.js
@@ -5,23 +5,22 @@ const _ = require("lodash");
 const { User, validate } = require("../models/user");
 
 router.post("/", async (req, res) => {
-  const { error } = validate(req.body); // destructure to get error property of result in above line
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   let user = await User.findOne({ email: req.body.email });
-  if (user) return res.status(400).send("User already registered.");
+  if (user)
+    return res
+      .status(400)
+      .send("A user with this email address is already registered.");
 
   user = new User(_.pick(req.body, ["username", "email", "password"]));
   const salt = await bcrypt.genSalt(10);
   user.password = await bcrypt.hash(user.password, salt);
-
-  console.log(user);
-  //insert into mongodb
   await user.save();
 
-  // const token = jwt.sign({ _id: user._id }, config.get('jwtPrivateKey'));
   const token = user.generateAuthToken();
-  // uses lodash to select properties to return in response
+
   res
     .header("x-auth-token", token)
     .header("access-control-expose-headers", "x-auth-token")
